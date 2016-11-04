39e0cafc8b61227f2dd86cf7e6ec6e9db7b9d85f
Omar Borquez <omarzaga@gmail.com>
2016-11-04 01:17:12 -0700

    if evaluador en concentrado

--- protected/views/indicadores/concentrado.php
+++ protected/views/indicadores/concentrado.php
@@ -10,28 +10,28 @@ $this->breadcrumbs=array(
 
 
 <div class="page-header">
-    
+
     <div class="row">
         <!-- Page header, center on small screens -->
         <h1 class="col-xs-12 col-sm-4 text-center text-left-sm"><i class="fa fa-icon page-header-icon"></i>&nbsp;&nbsp; Indicadores</h1>
         <div class="col-xs-12 col-sm-8 text-right">
         	<a href="<?echo Yii::app()->baseUrl?>/indicadoresPeriodos/create" class="btn btn-primary btn-labeled" "><span class="btn-label icon fa fa-plus"></span> Nuevo Periodo</a>
         	<a href="<?echo Yii::app()->baseUrl?>/indicadores/create" class="btn btn-primary btn-labeled" "><span class="btn-label icon fa fa-plus"></span> Nuevo indicador</a>
-            
+
         </div>
     </div>
 </div> <!-- / .page-header -->
 
 <div class="panel">
 	<div class="panel-body">
-	
+
 	<?foreach( $periodos as $periodo): ?>
-		
+
 		<div class="table-primary">
 			<div class="table-header">
 				<div class="table-caption">
 					<?= $periodo->periodo?>
-					
+
 				</div>
 			</div>
 			<table class="table table-bordered">
@@ -48,7 +48,7 @@ $this->breadcrumbs=array(
 				</thead>
 				<tbody>
 					<?foreach($periodo->valores as $valor):?>
-						<??>
+						<?if($valor->indicador):?>
 						<tr>
 							<td class="text-center"><img src="<?= Yii::app()->baseUrl.$valor->indicador->usuario->image?>" style="border-radius:50%; width:30px;"></td>
 							<td><?= $valor->indicador->usuario->nombre?></td>
@@ -62,16 +62,16 @@ $this->breadcrumbs=array(
 								<a href="<?= Yii::app()->baseUrl?>/indicadoresValores/update/<?=$valor->id?>" class="btn btn-warning"><i class="fa fa-edit"></i></a>
 							</td>
 						</tr>
+						<?endif?>
 					<?endforeach?>
 				</tbody>
 			</table>
-			
+
 		</div>
 
-		
-		
+
+
 	<?endforeach?>
-	
+
 	</div>
 </div>
-
